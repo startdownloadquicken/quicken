99b791ac5e7c90b4f95abc21d00c7df907c6007f
startdownloadquicken <jnvii87.48@gmail.com>
2026-01-30 16:23:15 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -5,9 +5,11 @@ release = '1.0'
 extensions = []
 
 templates_path = ['_templates']
-
 exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 
 html_static_path = ['_static']
+
+# 🔥 ADVANCED: Serve full HTML site from _static as root
+html_extra_path = ['_static']
